cbd2da2cb8498b25847a4536e94e9e48476833be
HennaHyen <henna.hyen97@kakaovx.com>
2022-08-08 17:11:56 +0900

    Branches Merge

--- app/src/main/java/com/example/prTest/SecondActivity.kt
+++ app/src/main/java/com/example/prTest/SecondActivity.kt
@@ -2,10 +2,21 @@ package com.example.prTest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.prTest.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_second)
+        setUpValue()
+        setUpEvent()
+    }
+    private fun setUpValue(){
+        //mBinding.secondRecyclerView.adapter
+    }
+    private fun setUpEvent(){
+
     }
 }
---
+++ app/src/main/java/com/example/prTest/SecoundAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.prTest
+
+import androidx.recyclerview.widget.RecyclerView
+
+class SecondAdapter(val mList: ArrayList<String>){
+}
