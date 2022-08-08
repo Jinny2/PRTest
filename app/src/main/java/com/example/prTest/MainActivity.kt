be68a5e4b2a4896f07ff0891fdb9fe90d0bfb456
HennaHyen <henna.hyen97@kakaovx.com>
2022-08-08 16:51:05 +0900

    안드로이드, MainActivity 버튼 추가

---
+++ app/src/main/java/com/example/prTest/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.prTest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.prTest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mIntent: Intent
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setUpEvent()
+    }
+
+    private fun setUpEvent() {
+        mBinding.btn1.setOnClickListener {
+        }
+        mBinding.btn2.setOnClickListener {
+            mIntent = Intent(this, SecondActivity::class.java)
+            startActivity(mIntent)
+        }
+    }
+}
