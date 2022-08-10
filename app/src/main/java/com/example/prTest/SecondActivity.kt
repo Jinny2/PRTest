dbf27a7aead7c3dcc7c5e947006271b6bda80009
HennaHyen <henna.hyen97@kakaovx.com>
2022-08-10 22:03:56 +0900

    [#2] Feat : RecyclerView 생성
    btn2 클릭시 Number List를 보여주는 Activity로 이동

--- app/src/main/java/com/example/prTest/SecondActivity.kt
+++ app/src/main/java/com/example/prTest/SecondActivity.kt
@@ -1,22 +1,34 @@
 package com.example.prTest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.prTest.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivitySecondBinding
+    private lateinit var mAdapter: SecondAdapter
+    private var mList: ArrayList<String> = arrayListOf()
+
+    //불필요한 주석, 주석 삭제 요청해주시면 수정 할 수 있게 달아논 주석입니다.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_second)
         setUpValue()
-        setUpEvent()
     }
-    private fun setUpValue(){
-        //mBinding.secondRecyclerView.adapter
-    }
-    private fun setUpEvent(){
 
+    private fun setUpValue() {
+        setData()
+        mAdapter = SecondAdapter(mList)
+        mBinding.secondRecyclerView.adapter = mAdapter
+        mBinding.secondRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setData() {
+        for (index in 1..100) {
+            mList.add(index.toString())
+        }
     }
 }
--- app/src/main/java/com/example/prTest/SecoundAdapter.kt
+++ app/src/main/java/com/example/prTest/SecoundAdapter.kt
@@ -1,6 +1,30 @@
 package com.example.prTest
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class SecondAdapter(val mList: ArrayList<String>){
+class SecondAdapter(val mList: ArrayList<String>) :
+    RecyclerView.Adapter<SecondAdapter.ItemViewHolder>() {
+    inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val numTxt = view.findViewById<TextView>(R.id.numTxt)
+        fun bind(item: String) {
+            numTxt.text = item
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val row = LayoutInflater.from(parent.context).inflate(R.layout.second_item, parent, false)
+        return ItemViewHolder(row)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(mList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
 }
