6f7a9b8b145148f659aab69a7947c4ed30fc80b8
Jinny <emhwang@kakaovx.com>
2022-08-10 18:20:26 +0900

    [#4] Feat : FirstDetailActivity 생성
    Item Click시 이동하는 화면인 FirstDetailActivity 생성

---
+++ app/src/main/java/com/example/prTest/first/detail/FirstDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.prTest.first.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_detail)
+    }
+}
