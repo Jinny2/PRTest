59651ce466ce8042bed0f6588758a32d75d9647b
Jinny <emhwang@kakaovx.com>
2022-08-10 18:11:19 +0900

    [#4] Feat : FirstAdapter 생성
    FirstAdapter 생성

--- app/src/main/java/com/example/prTest/first/adapter/FirstAdapter.kt
+++ app/src/main/java/com/example/prTest/first/adapter/FirstAdapter.kt
@@ -1,5 +1,25 @@
 package com.example.prTest.first.adapter
 
-class FirstAdapter {
-    
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.prTest.databinding.ItemFirstBinding
+import com.example.prTest.first.adapter.listener.ItemClickListener
+
+class FirstAdapter(
+    private val clickListener: ItemClickListener
+) : RecyclerView.Adapter<FirstViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FirstViewHolder {
+        return FirstViewHolder(
+            ItemFirstBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+            clickListener
+        )
+    }
+
+    override fun onBindViewHolder(holder: FirstViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount() = 30
 }
