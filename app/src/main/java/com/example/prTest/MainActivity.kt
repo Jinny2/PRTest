a6d49c4cab98291b70b8814db225a2bd47c83393
HennaHyen <henna.hyen97@kakaovx.com>
2022-08-11 10:40:31 +0900

    [#2] Refactor : Test 추석 추가
    추석을 추가함

--- app/src/main/java/com/example/prTest/MainActivity.kt
+++ app/src/main/java/com/example/prTest/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.databinding.DataBindingUtil
 import com.example.prTest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // 테스트용 주석 달기 !!
     private lateinit var mBinding: ActivityMainBinding
     private lateinit var mIntent: Intent
     override fun onCreate(savedInstanceState: Bundle?) {
