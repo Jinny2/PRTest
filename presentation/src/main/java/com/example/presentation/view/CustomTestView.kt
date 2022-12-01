713bbf35e0685f7e467442a2556f152d538cf657
jusung <jusungkim@kakakovx.com>
2022-12-01 17:14:22 +0900

    [feat] 컴포즈 테마 컬러값 및 라운드 처리 추가 (#27)

--- presentation/src/main/java/com/example/presentation/view/CustomTestView.kt
+++ presentation/src/main/java/com/example/presentation/view/CustomTestView.kt
@@ -1,3 +1,16 @@
+package com.example.presentation.view
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import com.example.presentation.view.theme.Purple200
+import com.example.presentation.view.theme.Purple500
+import com.example.presentation.view.theme.Purple700
+import com.example.presentation.view.theme.Shapes
+import com.example.presentation.view.theme.Teal200
+
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
@@ -18,3 +31,19 @@ private val LightColorPalette = lightColors(
     onSurface = Color.Black,
     */
 )
+
+@Composable
+fun JatpackComoseTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = com.example.presentation.view.theme.Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
