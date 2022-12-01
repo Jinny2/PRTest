6cb7c52fe0589c639a573a01ce9253b1c231817f
jusung <jusungkim@kakakovx.com>
2022-12-01 17:27:36 +0900

    [feat] 컴포즈 리스트 테스트 추가 (#27)

--- presentation/src/main/java/com/example/presentation/view/ComposeTestActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/ComposeTestActivity.kt
@@ -17,6 +17,37 @@ fun TestApp(content: @Composable () -> Unit) {
         }
     }
 }
+
+@Composable
+fun TestScreenContents(texts: List<String> = List(size = 1000) { "Hi Compose $it" }) {
+    val count = remember { mutableStateOf(value = 0) }
+
+    Column(modifier = Modifier.fillMaxHeight()) {
+
+        NameList(texts, Modifier.weight(weight = 0.9f))
+
+//        Column(modifier = Modifier.weight(weight = 1f)) {
+//            texts.forEach {
+//                Greeting(it)
+//                Divider(color = Color.Red)
+//            }
+//        }
+        CounterButton(count.value, Modifier.weight(weight = 0.1f)) { newCnt ->
+            count.value = newCnt
+        }
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        itemsIndexed(items = names) { index, item ->
+            Log.d("NameList", " index :$index , item : $item")
+            Greeting(item)
+            Divider(color = Color.Red)
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
