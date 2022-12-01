ca491cddf3842a2721bb64ae5895d799b57de326
jusung <jusungkim@kakakovx.com>
2022-12-01 17:29:43 +0900

    [feat] 컴포즈 리스트 클릭 이벤트 적용 추가 (#27)
    클릭시 숫자값 증가

--- presentation/src/main/java/com/example/presentation/view/ComposeTestActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/ComposeTestActivity.kt
@@ -1,3 +1,38 @@
+package com.example.presentation.view
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
 class ComposeTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -9,6 +44,28 @@ class ComposeTestActivity : AppCompatActivity() {
         }
     }
 }
+
+@Composable
+fun Greeting(name: String) {
+    var isSelected by remember {
+        mutableStateOf(value = false)
+    }
+    val backgroundColor by animateColorAsState(
+        if (isSelected) Color.Red else Color.Transparent
+    )
+
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .padding(10.dp)
+            .background(color = backgroundColor)
+            .clickable(onClick = {
+                isSelected = !isSelected
+            }),
+        style = MaterialTheme.typography.h1
+    )
+}
+
 @Composable
 fun TestApp(content: @Composable () -> Unit) {
     JatpackComoseTheme {
