48dd6a5814c560da4560de916f28929100696245
jusung <jusungkim@kakakovx.com>
2022-12-01 17:49:49 +0900

    [feat] presentation 모듈 그래들 파일  추가 (#28)

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,27 +1,28 @@
 plugins {
-    id ("com.android.library")
-    id ("org.jetbrains.kotlin.android")
-    id ("kotlin-kapt")
-    id ("dagger.hilt.android.plugin")
-    id ("org.jlleitschuh.gradle.ktlint")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("org.jlleitschuh.gradle.ktlint")
+    id("io.gitlab.arturbosch.detekt")
 }
 
 android {
-    namespace = "com.example.presentation"
-    compileSdk = AppConfig.compileSdk
+    compileSdk = Dependency.AppConfig.compileSdk
 
     defaultConfig {
-        minSdk = AppConfig.minSdk
-        targetSdk = AppConfig.targetSdk
-        versionCode = AppConfig.versionCode
-        versionName = AppConfig.versionName
+        minSdk = Dependency.AppConfig.minSdk
+        targetSdk = Dependency.AppConfig.targetSdk
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
@@ -39,38 +40,51 @@ android {
         compose = true
         dataBinding = true
     }
-}
 
+    composeOptions {
+        kotlinCompilerExtensionVersion = Dependency.AppConfig.compose_compiler
+    }
+
+    ktlint {
+        disabledRules.set(setOf("final-newline"))
+    }
+    namespace = "com.example.presentation"
+}
 
 dependencies {
     implementation(project(":data"))
     implementation(project(":domain"))
 
     // testUnit
-    testImplementation(Libraries.Test.JUNIT)
-    androidTestImplementation(Libraries.AndroidTest.TEST)
-    androidTestImplementation(Libraries.AndroidTest.TEST_CORE)
+    testImplementation(Dependency.Libraries.Test.JUNIT)
+    androidTestImplementation(Dependency.Libraries.AndroidTest.TEST)
+    androidTestImplementation(Dependency.Libraries.AndroidTest.TEST_CORE)
 
     // Coroutines
-    implementation(Libraries.COROUTINES.KOTLIN_COROUTINES)
+    implementation(Dependency.Libraries.COROUTINES.COROUTINES_KOTLINX)
+    implementation(Dependency.Libraries.COROUTINES.COROUTINES_VIEWMODEL)
 
     // Kakao SDK
-    implementation(Libraries.KAKAO.KAKAO_SDK)
+    implementation(Dependency.Libraries.KAKAO.KAKAO_SDK)
 
     // Hilt
-    implementation(Libraries.HLIT.HLIT)
-    kapt(Libraries.HLIT.HLIT_COMPILER)
+    implementation(Dependency.Libraries.HLIT.HLIT)
+    kapt(Dependency.Libraries.HLIT.HLIT_COMPILER)
 
     // KTX
-    implementation implementation (Libraries.KTX.CORE)
-    implementation implementation (Libraries.KTX.NAVIGATION_FRGMENT)
-    implementation implementation (Libraries.KTX.NAVIGATION_UI)
+    implementation(Dependency.Libraries.KTX.CORE)
+    implementation(Dependency.Libraries.KTX.NAVIGATION_FRGMENT)
+    implementation(Dependency.Libraries.KTX.NAVIGATION_UI)
 
     // AndroidX
-    implementation implementation (Libraries.AndroidX.APP_COMPAT)
-    implementation implementation (Libraries.AndroidX.MATERIAL)
-    implementation implementation (Libraries.AndroidX.CONSTRAINT_LAYOUT)
+    implementation(Dependency.Libraries.AndroidX.APP_COMPAT)
+    implementation(Dependency.Libraries.AndroidX.MATERIAL)
+    implementation(Dependency.Libraries.AndroidX.CONSTRAINT_LAYOUT)
 
     // compose
-    implementation (Libraries.COMPOSE.composeLibraries)
+    implementation(Dependency.Libraries.COMPOSE.COMPOSE_PREVIEW)
+    implementation(Dependency.Libraries.COMPOSE.COMPOSE_TOOL)
+    implementation(Dependency.Libraries.COMPOSE.COMPOSE_UI)
+    implementation(Dependency.Libraries.COMPOSE.COMPOSE_ACTIVITY)
+    implementation(Dependency.Libraries.COMPOSE.COMPOSE_MATERIAL)
 }
