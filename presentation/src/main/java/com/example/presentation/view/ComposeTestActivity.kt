ef5b63408fdbe88d7cec1db24f269e5aa2f0ea15
jusung <jusungkim@kakakovx.com>
2022-12-01 17:28:18 +0900

    [feat] 컴포즈 버튼 테스트 적용 추가 (#27)

--- presentation/src/main/java/com/example/presentation/view/ComposeTestActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/ComposeTestActivity.kt
@@ -48,6 +48,44 @@ fun NameList(names: List<String>, modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Composable
+fun NameCard(modifier: Modifier = Modifier) {
+    Row {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+        }
+        Column() {
+            Text(text = "안녕 하세요", fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@Composable
+fun CounterButton(count: Int, modifier: Modifier = Modifier, updateCount: (Int) -> Unit) {
+    Log.d("check", "CounterButton1")
+    Button(
+        onClick = { updateCount(count + 1) },
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(backgroundColor = if (count > 5) Color.Green else Color.LightGray)
+    ) {
+        Log.d("check", "Click1 :$count")
+        Text(text = "Clicked Count: $count")
+    }
+}
+
+@Composable
+fun CounterButton2(count: MutableState<Int>) {
+    Log.d("check", "CounterButton2")
+    Button(onClick = { count.value = count.value + 1 }) {
+        Log.d("check", "Click2 :${count.value}")
+        Text(text = "Clicked Count2: ${count.value}")
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
