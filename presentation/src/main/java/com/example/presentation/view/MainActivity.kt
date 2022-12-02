bae2f9dee8fb92659b01fded8b7b009680f238e4
jusung <jusungkim@kakakovx.com>
2022-12-02 15:52:31 +0900

    [feat] presentation MainActivity 추가 (#28)
    버튼3 이벤트에 카카오 로그인 기능 추가

---
+++ presentation/src/main/java/com/example/presentation/view/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.presentation.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.presentation.KakaoProvider
+import com.example.presentation.R
+import com.example.presentation.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mIntent: Intent
+    private lateinit var kakaoProvider: KakaoProvider
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        kakaoProvider = KakaoProvider(this)
+        setUpEvent()
+    }
+
+    private fun setUpEvent() {
+        mBinding.btn1.setOnClickListener {
+        }
+        mBinding.btn2.setOnClickListener {
+            mIntent = Intent(this, SecondActivity::class.java)
+            startActivity(mIntent)
+        }
+        mBinding.btn3.setOnClickListener {
+
+            CoroutineScope(Dispatchers.Main).launch {
+
+                kakaoProvider.kakaoLogin().collect { user ->
+
+                    Log.i("debugKakao", "kakaoLogin: $user")
+                    mIntent = Intent(baseContext, ComposeTestActivity::class.java)
+                    startActivity(mIntent)
+                    finish()
+                }
+            }
+        }
+    }
+}
